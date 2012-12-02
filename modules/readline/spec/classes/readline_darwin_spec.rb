89dd56d0084d5f88bff271b3dad477c1630814ca
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-02 12:08:10 -0800

    patch and readline

---
+++ modules/readline/spec/classes/readline_darwin_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe "readline" do
+  context "on Darwin" do
+    let(:facts) do
+      { :operatingsystem => 'Darwin' }
+    end
+
+    it "should call autotools with proper environment" do
+      environment = {
+        "CFLAGS" => "-arch i386 -arch x86_64",
+        "LDFLAGS" => "-arch i386 -arch x86_64",
+      }
+
+      should contain_autotools("readline").with_environment(environment)
+    end
+
+    it "should patch readline" do
+      should contain_patch("patch-readline")
+    end
+  end
+end
