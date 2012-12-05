9a28f96a4e97cf308d136ab98cc53eb34e5a5788
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-05 15:37:51 -0800

    Rename the arch package to the output path

--- modules/vagrant_installer/manifests/package/arch.pp
+++ modules/vagrant_installer/manifests/package/arch.pp
@@ -5,6 +5,7 @@
 class vagrant_installer::package::arch {
   $file_cache_dir = hiera("file_cache_dir")
   $arch_prefix = hiera("arch_prefix")
+  $dist_dir      = $vagrant_installer::params::dist_dir
   $staging_dir    = $vagrant_installer::params::staging_dir
   $vagrant_version = $vagrant_installer::params::vagrant_version
 
@@ -12,6 +13,9 @@ class vagrant_installer::package::arch {
   $pkgver  = $vagrant_version
   $setup_dir = "${file_cache_dir}/arch_setup"
   $source_package = "vagrant.tar.gz"
+
+  $final_output_path = "${dist_dir}/vagrant_${hardwaremodel}.pkg.tar.xz"
+  $script_renamer = "${file_cache_dir}/arch_renamer"
 
   #------------------------------------------------------------------
   # Setup the working directory we'll use
@@ -59,6 +63,21 @@ class vagrant_installer::package::arch {
     require => [
       Exec["tar-staging-dir"],
       File["${setup_dir}/PKGBUILD"],
+    ],
+  }
+
+  # Rename it over to our final path
+  util::script { $script_renamer:
+    content => template("vagrant_installer/package/arch_renamer.erb"),
+  }
+
+  # Rename it
+  exec { "rename-arch-pkg":
+    command => "${script_renamer} ${setup_dir} ${final_output_path}",
+    creates => $final_output_path,
+    require => [
+      Exec["makepkg"],
+      Util::Script[$script_renamer],
     ],
   }
 }
