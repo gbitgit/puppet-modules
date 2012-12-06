b01be0dfca6d2b9e8aa2586a2d013712323c877a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2012-12-06 11:37:35 -0800

    windows: Create the batch scripts for Vagrant

--- modules/vagrant_installer/manifests/staging/windows.pp
+++ modules/vagrant_installer/manifests/staging/windows.pp
@@ -4,8 +4,12 @@
 #
 class vagrant_installer::staging::windows {
   $embedded_dir     = $vagrant_installer::params::embedded_dir
+  $staging_dir      = $vagrant_installer::params::staging_dir
   $vagrant_revision = $vagrant_installer::params::vagrant_revision
 
+  #------------------------------------------------------------------
+  # Dependencies
+  #------------------------------------------------------------------
   class { "ruby::windows":
     install_dir => $embedded_dir,
   }
@@ -14,5 +18,20 @@ class vagrant_installer::staging::windows {
     embedded_dir => $embedded_dir,
     revision     => $vagrant_revision,
     require      => Class["ruby::windows"],
+  }
+
+  #------------------------------------------------------------------
+  # Bin wrappers
+  #------------------------------------------------------------------
+  # Batch wrapper so that Vagrant can be executed from normal cmd.exe
+  file { "${staging_dir}/bin/vagrant.bat":
+    content => template("vagrant_installer/windows_vagrant.bat.erb"),
+    require => Class["vagrant"],
+  }
+
+  # Normal Bash wrapper for Cygwin installations
+  file { "${staging_dir}/bin/vagrant":
+    content => template("vagrant_installer/vagrant.erb"),
+    require => Class["vagrant"],
   }
 }
