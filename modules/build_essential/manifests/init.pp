7da83d2136bf186e756f3ffffa4d35a9310be93a
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-01-27 22:59:50 -0800

    Add libtool to centos build essential

--- modules/build_essential/manifests/init.pp
+++ modules/build_essential/manifests/init.pp
@@ -11,7 +11,7 @@ class build_essential {
     }
 
     'CentOS': {
-      package { ["gcc", "make", "automake"]:
+      package { ["gcc", "make", "automake", "libtool"]:
         ensure => installed,
       }
     }
