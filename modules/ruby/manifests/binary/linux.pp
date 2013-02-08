a7c0dbb93e7e51ba4fe155caeaa21f9664a84434
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2013-02-07 19:47:55 -0800

    Install RubyGems from source on Ubuntu 10.04

--- modules/ruby/manifests/binary/linux.pp
+++ modules/ruby/manifests/binary/linux.pp
@@ -3,7 +3,23 @@ class ruby::binary::linux {
     ensure => installed,
   }
 
-  if $operatingsystem != 'Archlinux' {
+  if $lsbdistid == 'Ubuntu' and $lsbmajdistrelease == '10' {
+    # On Ubuntu 10 we need to install RubyGems from scratch, because
+    # the one they have in packages are too old. We just use a script
+    # for this because of laziness.
+    util::script { "install-rubygems":
+      path    => "/usr/local/bin/install_rubygems_from_source",
+      content => template("ruby/install_rubygems_from_source.sh.erb"),
+    }
+
+    exec { "/usr/local/bin/install_rubygems_from_source":
+      unless  => "test -f /usr/bin/gem",
+      require => [
+        Package["ruby"],
+        Util::Script["install-rubygems"],
+      ],
+    }
+  } elsif $operatingsystem != 'Archlinux' {
     package { "rubygems":
       ensure => installed,
     }
@@ -21,6 +37,12 @@ class ruby::binary::linux {
 
     'CentOS': {
       package { "ruby-devel":
+        ensure => installed,
+      }
+    }
+
+    'Ubuntu': {
+      package { "ruby-dev":
         ensure => installed,
       }
     }
